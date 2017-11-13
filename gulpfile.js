11fb8359f9f1ea2090dac9d787e73a57ee0a73d5
Scotty Vernon <scott.vernon@gmail.com>
2017-11-13 22:18:49 +0000

    Initial commit.

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var svgmin = require('gulp-svgmin');
+var svgstore = require('gulp-svgstore');
+var path = require('path');
+
+gulp.task('default', function () {
+    return gulp.src('svgs/*.svg')
+        .pipe(svgmin(function (file) {
+            var prefix = path.basename(file.relative, path.extname(file.relative));
+            return {
+                plugins: [
+                    {
+                        cleanupIDs: {
+                            prefix: prefix + '-',
+                            minify: true
+                        }
+                    },
+                    {
+                        removeTitle: true
+                    },
+                    {
+                        removeDoctype: true
+                    },
+                    {
+                        removeComments: true
+                    }
+                ]
+            }
+        }))
+        .pipe(svgstore())
+        .pipe(gulp.dest('./dest'));
+});
